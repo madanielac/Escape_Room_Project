08b45c6b8b646da6f2c5f82f9a2833be4afe0618
kmlee22 <54556633+kmlee22@users.noreply.github.com>
2019-12-05 15:15:31 -0800

    Add files via upload

---
+++ Final Project/Key.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Dec  5 15:12:49 2019
+
+@author: Katie Lee
+"""
+
+def Key():
+    keyphrase = input('Enter the keyphrase: _ _ _ _ _ _ _')
+    if keyphrase == 'boolean':
+        return True
+    else:
+        return False
+    
