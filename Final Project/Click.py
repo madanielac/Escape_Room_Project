b0aba4b0d7f0b9101ac84ce6801c6e1d4d00ed74
kmlee22 <54556633+kmlee22@users.noreply.github.com>
2019-12-05 15:32:00 -0800

    Update Click.py

--- Final Project/Click.py
+++ Final Project/Click.py
@@ -11,17 +11,17 @@ def click_object(self):
     win = self.window
     coordinate = win.checkMouse()
     if coordinate in self.face:
-        choose_element = jed
+        choose_element = 'jed'
     if coordinate in self.projector:
-        choose_element = screen
+        choose_element = 'screen'
     if coordinate in self.board_front:
-        choose_element = front_board
+        choose_element = 'front_board'
     if coordinate in self.win_draw:
-        choose_element = win_drawing
+        choose_element = 'win_drawing'
     if coordinate in self.board_back:
-        choose_element = back_board
+        choose_element = 'back_board'
     if coordinate in self.table:
-        choose_element = table
+        choose_element = 'table'
     if coordinate in self.door:
-        choose_element = door
+        choose_element = 'door'
     return choose_element
