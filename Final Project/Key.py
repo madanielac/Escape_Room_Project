eac1e683dbc8c673d129f6fa914c64cd43b7d55f
kmlee22 <54556633+kmlee22@users.noreply.github.com>
2019-12-05 15:29:21 -0800

    Update Key.py

--- Final Project/Key.py
+++ Final Project/Key.py
@@ -5,6 +5,8 @@ Created on Thu Dec  5 15:12:49 2019
 @author: Katie Lee
 """
 
+#checking to see if the keyphrase is correct once the players reach the door of the escape room
+
 def Key():
     keyphrase = input('Enter the keyphrase: _ _ _ _ _ _ _')
     if keyphrase == 'boolean':
